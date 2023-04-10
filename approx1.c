4a44dc1f005a38c84ba0bc23deef005aba6c45d4
Eyub Ryze <100705373+Eyub4k@users.noreply.github.com>
2023-04-10 03:44:45 +0200

    Add files via upload

---
+++ approx1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct{   //on definit une structure pour pouvoir avoir en sortie notre a et b en sortie lors du calcule d'approxiamtion vue dans l'explication des methodes
+    double a;  // donc on definit double a et b, on definit ce type car nous pouvons avoir  un entier ou un float
+    double b;
+}coeff;
+
+coeff calculAB (double *x, double *y,int n){       // on a donc en entrée x et y definit dans les jeux d'essaie et n qui definit combien de point nous avons
+    double sommeX = 0;       // initialisation de la Somme ainsi que du produit des x et y à 0
+    double sommeY= 0;
+    double produitXY =0;
+    int i, j;                // definition de i,j et de resultat ;
+    double resultat;
+    coeff z;
+    for (i=0; i<n; i++){
+        sommeX += x[i];                      // Notre boucle ici calcule la somme des x et y et fait aussi le produit des x et y entre eux
+        sommeY += y[i];
+        produitXY += (x[i] * y[i]);
+    }
+
+
+    for (i=0; i<n; i++){
+        printf("%lf ", y[i]);                                      // on affiche ce que l'on a donc dans notre tableau de y
+    }
+    resultat = ( ( sommeX * sommeY )/ n ) - produitXY;              // nous affectons donc une fois : la (somme des x) multiplié à (somme des y) divisé a le nombre de point - prduit des x et x
+                                                                       // vue dans la formule pour trouver a dans la rubrique rapelle des methodes
+    printf("\n");
+    printf("sommeX = %lf\n", sommeX);
+    printf("sommeY = %lf\n", sommeY);
+    printf("produitXY = %lf\n", produitXY);
+    printf("a1 = %lf\n", resultat);
+
+    resultat = resultat / ( ( (sommeX * sommeX)/n ) - (sommeX * sommeX) );       // on finit donc la methode pour trouver a car dans resultat nousa vons juste calculer le numerateur,
+                                                                                 // donc nous calculons a en ajoutant le denomianteur : (sommeX * sommeX)/n ) - (sommeX * sommeX)
+    printf("\na2 = %lf\n", resultat);
+    z.a = resultat;                                                              // on a affecte donc resultat à a dans la struct
+
+    resultat = ( sommeY / n) - ( ( z.a * sommeX )/n );                           // on utilise resultat car on a pas cree d'autre variable, nous calculons b vue dans la methode avec
+    printf("\nb = %lf", resultat);                                               // le a calculé avant et on finit pas affecter une fois de plus resultat à b dans la struct.
+    z.b = resultat;
+    return z;
+}
+
+
+
+int main(){
+
+    int n, i;
+    printf("Combien de points ?");
+    scanf("%d",&n);
+    double *x = malloc ( (n-1)*sizeof(double));
+    double *y = malloc ( (n-1)*sizeof(double));                                 // on va creer des tableau de n taille demandé dans les jeux d'essaies
+    double valeur;
+    coeff z;
+
+    for(i = 0 ; i<n ; i++){
+        printf("Quelle valeur pour x[%d]\n ",i);
+        scanf("%lf", &valeur);                                                  // on remplie manuellement les x
+        x[i] = valeur;
+    }
+
+    for(i=0;i<n;i++){
+        printf("Quelle valeur pour y[%d]\n ",i);                               // on remplie manuellement les y
+        scanf("%lf",&valeur);
+        y[i]=valeur;
+    }
+
+
+    z = calculAB(x,y,n);                                                       // on calcule notre coef a et b pour pouvoir deduire ensuite la fonction
+}
---
+++ approx2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>                                      // pour pouvoir avoir un ajustement exponentiel, nousa vons vue dans les methode qu'il faut mettre les y en ln
+
+typedef struct{                                        //  et ainsi pour avoir y = ce^(dx) , on sait que c vaut exp(b) et d vaut a;
+    double c;
+    double d;
+}coeff;                    //on definit une structure pour pouvoir avoir en sortie notre c et d en sortie lors du calcule d'approxiamtion vue dans l'explication des methodes
+                          // donc on definit double c et d, on definit ce type car nous pouvons avoir  un entier ou un float
+
+coeff calculCD (double *x, double *y,int n){
+    double sommeX = 0;                             // on a donc en entrée x et y definit dans les jeux d'essaie et n qui definit combien de point nous avons
+                                                   // initialisation de la Somme ainsi que du produit des x et y à 0
+    double sommeY = 0;
+    double produitXY =0;
+    int i, j;                         // definition de i,j et de a et b
+    double a, b;
+    coeff z;
+    for (i=0; i<n; i++){
+        sommeX += (x[i]);                     // calcule des sommes de x et (ln de y) puis les produit des x et y
+        sommeY += log(y[i]);
+        produitXY += (x[i] * y[i]);
+    }
+    printf("x[ ] : ");
+    for (i=0; i<n; i++){
+        printf("%lf ", x[i]);                // affiche des nouveaux tab de x et y
+    }
+    printf("\ny[ ] : ");
+    for (i=0; i<n; i++){
+        printf("%lf ", y[i]);
+    }
+    a = ( ( sommeX * sommeY )/ n ) - produitXY;            // premiere partie du calcule de a (numerateur) avec la methode vue dans les presentations
+
+    printf("\n");
+    printf("sommeX = %lf\n", sommeX);
+    printf("sommeY = %lf\n", sommeY);
+    printf("produitXY = %lf\n", produitXY);
+    printf("a = %lf", a);
+
+    a = a / ( ( (sommeX * sommeX)/n ) - (sommeX * sommeX) );       // on termine le calcule de a en ajoutant le denominateur vue dans la presentation des methodes
+    printf("\nd = %lf", a);
+    z.d = a;                                                 // affectaztion de a dans d (methode dans les presentations)
+
+    b = ( sommeY / n) - ( ( z.d * sommeX )/n );               // calcule de b avec ola methode vue dans les presentations des methodes avec d qui vaut a
+    printf("\nb = %lf", b);
+    z.c = exp(b);                                              // et affection de exponentiel de b dans c ( expliqué dans les presentations)
+    printf("\nc = %lf", z.c);
+
+    return z;
+}
+
+
+
+int main(){
+
+    int n, i;
+    printf("Combien de points ?");
+    scanf("%d",&n);
+    double *x = malloc ( (n-1)*sizeof(double));
+    double *y = malloc ( (n-1)*sizeof(double));
+    double valeur;                                                    // on va creer des tableau de n taille demandé dans les jeux d'essaies
+    coeff z;
+
+    for(i = 0 ; i<n ; i++){
+        printf("Quelle valeur pour x[%d]\n ",i);
+        scanf("%lf", &valeur);
+        x[i] = valeur;                                            // on remplie manuellement les x
+    }
+
+    for(i=0;i<n;i++){
+        printf("Quelle valeur pour y[%d]\n ",i);
+        scanf("%lf",&valeur);                                       // on remplie manuellement les y
+        y[i]=valeur;
+    }
+
+
+    z = calculCD(x,y,n);                                              // on calcule notre coef c et d pour pouvoir deduire ensuite la fonction
+}
